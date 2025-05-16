0f3c563ef77cd4cea7e3f616b781d62688b68c52
Christelle Jean-Jacques <2153876@collegemv.qc.ca>
2025-05-15 21:59:47 -0400

    Ajout de la section expérience de travail, mais ce n'est pas encore fini

---
+++ src/Components/Experience.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Experience.css'
+
+function Experience() {
+  return (
+    <div className="Container">
+      <h1>Expérience de travail</h1>
+      <div className="experience-item" >
+        <h3>Comis au produit frais</h3>
+      </div>
+      <div className="experience-item">
+        <h3>
+          comis au service à la clientèle
+        </h3>
+      </div>
+      <div className="experience-item"> 
+        <h3>Remplacement CSSPI</h3>
+      </div>
+      <div className="experience-item">
+        <h3>Concierge</h3>
+      </div>
+    
+    </div>
+  )
+}
+
+export default Experience
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ProfileSection from '../Components/ProfileSection'
 import './Home.css'
 import CardList from '../Components/CardList'
+import Experience from '../Components/Experience'
 
 
 function Home() {
   return (
     <div>
         <ProfileSection/>
+        <Experience/>
         <CardList/>
     </div>
   )
